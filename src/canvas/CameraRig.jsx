1d064f107233bd0f7ff888ea7b2d44ffe9075e84
Charlie Carter <charliec1665@gmail.com>
2023-10-08 23:36:36 -0400

    began tshirt model positioning

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -8,6 +8,35 @@ import state from '../store';
 const CameraRig = ({ children }) => {
     // create ref to move camera closer by updating state
     const group = useRef();
+    const snap = useSnapshot(state);
+
+    useFrame((state, delta) => {
+        // media breakpoints for shirt model
+        const isBreakpoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <= 600;
+
+        // set initial position of model
+        let targetPosition = [-0.4, 0, 2];
+        if(snap.intro) {
+            if(isBreakpoint) targetPosition = [0, 0, 2];
+            if(isMobile) targetPosition = [0, 0.2, 2.5];
+        } else {
+            if(isMobile) targetPosition = [0, 0, 2.5]
+            else targetPosition = [0, 0, 2];
+        }
+
+        // set model camera position
+        easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+
+        // set model rotation smoothly
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y / 10, -state.pointer.x / 5, 0],
+            0.25,
+            delta
+        )
+    })
+
 
   return <group ref={group}>{children}</group>
 }
